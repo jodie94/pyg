3d84ee3d40b98db2614d22a9b9a03790613ad791
Jodie <1511590602@qq.com>
2019-01-08 19:03:20 +0800

    第一天项目

---
+++ pyg-parent/pyg-sellergoods-interface/src/main/java/com/pyg/service/BrandService.java
@@ -0,0 +1,13 @@
+package com.pyg.service;
+
+import com.pyg.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * Created by crowndint on 2018/11/27.
+ */
+public interface BrandService {
+
+    public List<TbBrand> findAll();
+}
---
+++ pyg-parent/pyg-sellergoods-service/src/main/java/com/pyg/service/impl/BrandServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pyg.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pyg.mapper.TbBrandMapper;
+import com.pyg.pojo.TbBrand;
+import com.pyg.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by crowndint on 2018/11/27.
+ */
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    @Autowired
+    private TbBrandMapper brandMapper;
+
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null);
+    }
+}
